c4267bf5485fc756d12340b42a6fd6055f396a48
Atru8729 <a.truskauskas1@gmail.com>
2021-07-28 19:56:23 +0300

    start

--- Kindergarden.js
+++ Kindergarden.js
@@ -1,5 +1,20 @@
 class Kindergarden {
+    constructor() {
+        this.kidsLimit = 3;
+        this.Kindergarden = ('Barsukai');
+    }
 
+    intro() {
+        console.log(`"${this.Kindergarden}" can accept ${this.kidsLimit} kids.`);
+    }
+    updateKidsLimit(updatedKidsLimit) {
+        this.updatedKidsLimit = updatedKidsLimit;
+        console.log(`"${this.Kindergarden}" can accept ${updatedKidsLimit} kids now.`);
+    }
+    addKid(kidName) {
+        this.kidName = kidName;
+        console.log(`${this.kidName} has entered "${this.Kindergarden}" kindergarden.`);
+    }
 }
 
 module.exports = Kindergarden;
